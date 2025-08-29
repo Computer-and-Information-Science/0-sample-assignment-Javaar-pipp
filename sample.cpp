ba367e1123c05ceac88ecad97edab1fafa1d26d7
Javaar-pipp <javaar.pippinger@student.northampton.edu>
2025-08-29 23:58:10 +0000

    Assignment #0

--- sample.cpp
+++ sample.cpp
@@ -4,4 +4,16 @@ using namespace std;
 
 int main () {
 
+int a;
+int b;
+
+cout << "Enter first integer: ";
+cin >> a;
+cout << "Enter second integer: ";
+cin >> b;
+cout << endl;
+cout << "The sum is " << a + b << ".";
+
+return 0;
+
 }
